3a954add417e93d3d7fd8b894141705a07ab3a5d
Wilco van Beijnum <wilcovanbeijnum@gmail.com>
2020-06-03 21:30:16 +0200

    Add users endpoint

--- dsa_api/urls.py
+++ dsa_api/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework import routers
+from . import views
 
 
 router = routers.DefaultRouter()
+router.register('users', views.UserView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('activities.urls'))
+    path('', include('activities.urls')),
+    path('', include(router.urls))
 ]
